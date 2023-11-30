ce565420bcb8583f81e3c86c201613a1dc8158f7
Edilson Almeida <edifilho1409@outook.com>
2023-11-30 07:02:50 -0300

    new: SideBard
    Sidebar criada, juntamente com seu componente de avatar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { Header } from "./components/Header/header";
+import { Sidebar } from "./components/SideBar/sideBar";
 import "./global.css";
+import styles from "./app.module.css";
 
 export function App() {
   return (
     <div>
       <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+      </div>
     </div>
   );
 }
---
+++ src/components/Avatar/avatar.tsx
@@ -0,0 +1,15 @@
+import { ImgHTMLAttributes } from "react";
+import styles from "./avatar.module.css";
+
+interface AvatarProps extends ImgHTMLAttributes<HTMLImageElement> {
+  hasBorder?: boolean;
+}
+
+export function Avatar({ hasBorder = true, ...props }: AvatarProps) {
+  return (
+    <img
+      className={hasBorder ? styles.avatarWithBorder : styles.avatar}
+      {...props}
+    />
+  );
+}
